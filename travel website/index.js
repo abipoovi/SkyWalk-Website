031afc7fd4d600d4a24d4a0404902e1f7c8bfeed
Abinaya <161562559+abipoovi@users.noreply.github.com>
2026-06-19 14:32:20 +0530

    Add files via upload

---
+++ travel website/index.js
@@ -0,0 +1,15 @@
+AOS.init(
+    {
+        once:false,
+        duration:2000,
+        dealy:300,
+        easing:'ease',
+
+    
+    });
+
+const menuBtn=document.querySelector(".menu-btn");
+const navlinks=document.querySelector(".nav-links");
+menuBtn.addEventListener("click",()=>{
+    navlinks.classList.toggle("active");
+})
